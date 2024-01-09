086fc968aee897ac56a1e8fc876ef38cc5232db3
iom996 <metelskiyivantop@gmail.com>
2024-01-09 17:17:29 +0100

    fix

--- pm.py
+++ pm.py
@@ -10,11 +10,11 @@ class PmMod(loader.Module):
     strings = {"name": "pm",
                 "ls": f"<b>please go to pm<b>",
                 "ls_ru": f"<b>пожалуйста напиши мне в лс<b>",
-                "ls_ukr": f"<b>будь ласка напишіть мені у пс<b>",
+                "ls_ukr": f"<b>будь ласка, напишіть мені у пс<b>",
                 }
 
     async def pmcmd(self, message: Message) -> None:
-        """<ru or en>"""
+        """<ru or en or none>"""
         lang = utils.get_args_raw(message)
         if lang == "ru" :
             await utils.answer(message, self.strings("ls_ru"))
