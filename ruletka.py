a05c92e8fa050456e1852ba56de0cd7c4b5fcec6
iom996 <metelskiyivantop@gmail.com>
2024-01-09 17:32:16 +0100

    new module(beta)

---
+++ ruletka.py
@@ -0,0 +1,22 @@
+from hikkatl.types import Message
+from .. import loader
+import random
+
+
+@loader.tds
+class MyModule(loader.Module):
+    """русская рулетка"""
+
+    strings = {"name": "ruletka", "loose": f"<b>you loose!<b>", "win": f"<b>you won!<b>"}
+    strings_ru = {"loose": f"<b>вы проиграли!<b>", "win": f"<b>вы победили!<b>"}
+
+    @loader.command(
+        )
+    async def rulcmd(self, message: Message):
+        """any number 1-6"""
+        numo = random(random.randint(1-6))
+        num = utils.get_args_raw(message)
+        if num == numo :
+            await utils.answer(message, self.strings("loose"))
+        else:
+            await utils.answer(message, self.strings("win"))
