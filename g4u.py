ba1bf7195353db6e4d00ddcc22b13240495f9860
iom996 <metelskiyivantop@gmail.com>
2024-01-09 21:58:32 +0100

    new module

---
+++ g4u.py
@@ -0,0 +1,22 @@
+from .. import loader, utils
+from telethon.tl.types import Message  # type: ignore
+
+#meta developer:@imloveonegirl
+
+@loader.tds
+class googleMod(loader.Module):
+    """загугли..."""
+
+    strings = {"name": "gogl",
+               "noag": "no args"}
+    strings_ru = {"noag": "нету запроса",}
+
+    async def goglcmd(self, message: Message) :
+        """gogl"""
+        args = utils.get_args_raw(message)
+        if not args:
+            await utils.answer(message, self.strings("no args"))
+            return
+        g = args.replace(" ", "+")
+        gogl = f"https://googlegiksearch.github.io/?q={g}"
+        await utils.answer(message, gogl)
